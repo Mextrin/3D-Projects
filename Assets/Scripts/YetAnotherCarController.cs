10f298f2a2ad6ba0aef6bdf63673d3c95cb1124f
Mextrin <seba3552@hotmail.com>
2016-09-05 07:23:33 +0200

    Birth to the car.
    Made the car movement based on acceleration instead of calculating the
    engine RPM.

--- Assets/Scripts/YetAnotherCarController.cs
+++ Assets/Scripts/YetAnotherCarController.cs
@@ -6,41 +6,44 @@ public enum AccelerationType { FrontWheelDrive, RearWheelDrive, FourWheelDrive }
 public class YetAnotherCarController : MonoBehaviour
 {
     public WheelRolling[] wheelTorque = new WheelRolling[4];
-    public AccelerationType driveType;
-    public float finalTorque;
-    public float maxRPM, idleRPM;
-    public float currentRPM;
+    //public AccelerationType driveType;
+    //public float finalTorque;
+    //public float maxRPM, idleRPM;
+    //public float currentRPM;
     public float acceleration, inputAcceleration;
-    public float outputForce;
-    public bool clutch;
-
-    public int currentGear;
-    public float gearRRatio, gear1Ratio, gear2Ratio, gear3Ratio, gear4Ratio, gear5Ratio, gear6Ratio;
-    public float finalDriveAxleRatio;
-    float[] gear;
-
+    public float velocity;
+    //public bool clutch;
+    //
+    //public int currentGear;
+    //public float gearRRatio, gear1Ratio, gear2Ratio, gear3Ratio, gear4Ratio, gear5Ratio, gear6Ratio;
+    //public float finalDriveAxleRatio;
+    //float[] gear;
+    //
     public float maxAngle;
     public float currentAngle;
     public float steeringInput;
+    //
+    //Rigidbody rigidbody;
 
-    Rigidbody rigidbody;
+    float timeStart, currentTime;
+    bool stillholding;
 
     // Use this for initialization
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
-
-        gear = new float[]
-            {
-                gearRRatio,
-                0,
-                gear1Ratio,
-                gear2Ratio,
-                gear3Ratio,
-                gear4Ratio,
-                gear5Ratio,
-                gear6Ratio,
-            };
+        //rigidbody = GetComponent<Rigidbody>();
+        //
+        //gear = new float[]
+        //    {
+        //        gearRRatio,
+        //        0,
+        //        gear1Ratio,
+        //        gear2Ratio,
+        //        gear3Ratio,
+        //        gear4Ratio,
+        //        gear5Ratio,
+        //        gear6Ratio,
+        //    };
     }
 
     // Update is called once per frame
@@ -50,77 +53,112 @@ public class YetAnotherCarController : MonoBehaviour
         inputAcceleration = Input.GetAxis("Acceleration");
         steeringInput = Input.GetAxis("Steering");
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (currentGear < 6)
-            {
-                currentGear++;
-            }
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            if (currentGear > -1)
-                currentGear--;
-
-        }
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    if (currentGear < 6)
+        //    {
+        //        currentGear++;
+        //    }
+        //
+        //}
+        //
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    if (currentGear > -1)
+        //        currentGear--;
+        //
+        //}
 
         //Focus point
         //Find a way to slow down acceleration / gear without affecting the RPM
-        acceleration = (inputAcceleration * (maxRPM - idleRPM)) / (gear[currentGear + 1]);
-        currentRPM = idleRPM + (acceleration * gear[currentGear + 1]);
-        finalTorque = currentRPM / gear[currentGear + 1] * finalDriveAxleRatio;
+        //acceleration = (inputAcceleration * (maxRPM - idleRPM)) / (gear[currentGear + 1]);
+        //currentRPM = idleRPM + (acceleration * gear[currentGear + 1]);
+        //finalTorque = currentRPM / gear[currentGear + 1] * finalDriveAxleRatio;
 
         currentAngle = maxAngle * steeringInput;
-        
 
-        if (!clutch && inputAcceleration > 0 && currentGear != 1)
+        //Speed in m/s
+        //Acceleration in m/s^2
+
+        //Time = sq(v / a)
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            //Speed = RPM
+            if (!stillholding)
+            {
+                stillholding = true;
+                currentTime = Mathf.Sqrt(velocity / acceleration);
+                timeStart = Time.time;
+                
+            }
+            float t = (Time.time - timeStart) + currentTime;
+            velocity = acceleration * (t * t);
+            Debug.Log("             " + (velocity * 60 * 60) / 1000 + " km/h");
         }
         else
         {
-            //RPM = Speed
+            if (stillholding)
+            {
+                stillholding = false;
+                currentTime = Mathf.Sqrt(velocity / acceleration);
+                timeStart = Time.time;
+            }
+            if (velocity > .01f)
+            {
+                float t = Time.time - (timeStart + currentTime);
+                velocity = acceleration * (t * t);
+
+            }
         }
 
-        switch (driveType)
-        {
-            case AccelerationType.FrontWheelDrive:
-                for (int i = 0; i < 2; i++)
-                {
-                    wheelTorque[i].spinRate = finalTorque / 2;
-                }
-                break;
 
-            case AccelerationType.RearWheelDrive:
-                for (int i = 2; i < 4; i++)
-                {
-                    wheelTorque[i].spinRate = finalTorque / 2;
-                }
-                break;
 
-            case AccelerationType.FourWheelDrive:
-                for (int i = 0; i < 4; i++)
-                {
-                    wheelTorque[i].spinRate = finalTorque / 4;
-                }
-                break;
-        }
+        //if (!clutch && inputAcceleration > 0 && currentGear != 1)
+        //{
+        //    //Speed = RPM
+        //}
+        //else
+        //{
+        //    //RPM = Speed
+        //}
 
-        outputForce = 0;
-        for (int i = 0; i < wheelTorque.Length; i++)
-        {
-            outputForce += wheelTorque[i].spinTorque;
-        }
+        //switch (driveType)
+        //{
+        //    case AccelerationType.FrontWheelDrive:
+        //        for (int i = 0; i < 2; i++)
+        //        {
+        //            wheelTorque[i].spinRate = finalTorque / 2;
+        //        }
+        //        break;
+        //
+        //    case AccelerationType.RearWheelDrive:
+        //        for (int i = 2; i < 4; i++)
+        //        {
+        //            wheelTorque[i].spinRate = finalTorque / 2;
+        //        }
+        //        break;
+        //
+        //    case AccelerationType.FourWheelDrive:
+        //        for (int i = 0; i < 4; i++)
+        //        {
+        //            wheelTorque[i].spinRate = finalTorque / 4;
+        //        }
+        //        break;
+        //}
 
-        wheelTorque[0].rollSpeed = outputForce;
-        wheelTorque[1].rollSpeed = -outputForce;
-        wheelTorque[2].rollSpeed = outputForce;
-        wheelTorque[3].rollSpeed = -outputForce;
+        //outputForce = 0;
+        //for (int i = 0; i < wheelTorque.Length; i++)
+        //{
+        //    outputForce += wheelTorque[i].spinTorque;
+        //}
+        
 
-        //rigidbody.AddForce(transform.forward * outputForce * 100);
-        //transform.Translate(transform.forward * (outputForce) * Time.deltaTime);
+        wheelTorque[0].rollSpeed = velocity;
+        wheelTorque[1].rollSpeed = -velocity;
+        wheelTorque[2].rollSpeed = velocity;
+        wheelTorque[3].rollSpeed = -velocity;
+
+        transform.Rotate(transform.up * currentAngle * Time.deltaTime);
+        transform.Translate(transform.forward * velocity * Time.deltaTime, Space.World);
     }
 
 }
