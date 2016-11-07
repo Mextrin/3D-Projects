617f9a308d10c71c4a4a2c836405eb45cd033d16
Mextrin <seba3552@hotmail.com>
2016-11-07 05:19:21 +0100

    Glitched engine.
    - Experimental methods to fix the wrong HP and Torque calculations.
    - Preparation for AI

--- Assets/Scripts/RPMCarController.cs
+++ Assets/Scripts/RPMCarController.cs
@@ -10,12 +10,16 @@ public class RPMCarController : MonoBehaviour
     public WheelRolling[] wheel = new WheelRolling[4];
     public AccelerationType driveType;
     public float currentTorque, maxTorqueAtRPM;
+    public float torqueAtRPM, torquePoint;
+
     public float maxRPM, idleRPM;
     public float currentRPM;
 
     public float gasInput, brakeInput;
 
-    public float horsepower;
+    public float currentHP;
+    public float horsepowerPoint;
+    public float HPAtRPM;
     public float acceleration;
     public float brakePower;
     public float velocity;
@@ -28,7 +32,7 @@ public class RPMCarController : MonoBehaviour
     public float maxAngle;
     public float currentAngle;
     public float steeringInput;
-    
+
     //Unity components
     Rigidbody rigidbody;
     public Text speedText, gearText;
@@ -41,7 +45,7 @@ public class RPMCarController : MonoBehaviour
     {
         //Torque in lb.ft
         rigidbody = GetComponent<Rigidbody>();
-        
+
         gear = new float[]
             {
                 gearRRatio,
@@ -67,7 +71,7 @@ public class RPMCarController : MonoBehaviour
                 currentGear++;
             UpdateGear();
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             if (currentGear > -1)
@@ -80,7 +84,7 @@ public class RPMCarController : MonoBehaviour
             float currentRotation = transform.rotation.y;
             transform.eulerAngles = new Vector3(0, currentRotation, 0);
         }
-        
+
         //Calculate RPM
         currentRPM = ((velocity / 0.35f) * gear[currentGear + 1] * finalDriveAxleRatio * 60) / (2 * Mathf.PI);
 
@@ -96,16 +100,19 @@ public class RPMCarController : MonoBehaviour
 
         RPM.fillAmount = (currentRPM / maxRPM) * 0.75f;
         //RPM.fillAmount = ((CalculateTorque(maxRPM) / maxTorqueAtRPM)) * 0.75f;
-        
+
         //Calculate resistance forces
         float totalDragResistance = -dragResistance * velocity * Mathf.Abs(velocity);
         float totalRollResistance = -rollResistance * velocity;
 
         //Set brake value
-        float brake = Mathf.Abs(velocity) > 0.1f ? brakePower: 0;
+        float brake = Mathf.Abs(velocity) > 0.1f ? brakePower : 0;
 
-        maxTorqueAtRPM = CalculateTorque(currentRPM);
-        horsepower = maxTorqueAtRPM * currentRPM / 5252;
+        //Calculate acceleration
+        //horsepower = (maxTorqueAtRPM * currentRPM) / 5252;
+        currentHP = FindValue(currentRPM, HPAtRPM, horsepowerPoint);
+        maxTorqueAtRPM = FindValue(currentRPM, torqueAtRPM, torquePoint);
+        //maxTorqueAtRPM = CalculateTorque(currentRPM);
         currentTorque = (gasInput * maxTorqueAtRPM) / gear[currentGear + 1];
 
         float driveForce = currentTorque /** gear[currentGear + 1]*/ * finalDriveAxleRatio * 0.7f;// / 0.33f;
@@ -115,25 +122,42 @@ public class RPMCarController : MonoBehaviour
         velocity += acceleration;
         speedText.text = ((int)Mathf.Abs((velocity * 60 * 60) / 1000)).ToString();
 
-
         float wheelRollSpeed = ((currentTorque * gear[currentGear + 1]) / 60) * 360 * Mathf.Deg2Rad;
 
+
         //rigidbody.AddForce(transform.forward * outputForce, ForceMode.Force);
-        transform.Translate(transform.forward * velocity * Time.deltaTime, Space.World);
+        //transform.Translate(transform.forward * velocity * Time.deltaTime, Space.World);
 
         currentAngle = (maxAngle * steeringInput);
         transform.Rotate(transform.up * currentAngle * Time.deltaTime);
-        
+
         //wheel[0].rollSpeed = driveTorque;
 
+    }
+
+    float FindValue(float currentValue, float pointedValue, float peekPoint)
+    {
+        //RPM to RPM by HP ----- Lerp(RPM, RPM, HP)
+        //return currentRPM / HPAtRPM;
+
+        float t = (currentValue / pointedValue);
+
+        float ULerpedValue = (1.0f - t) * 0 + t * peekPoint;
+
+        //Debug.Log("                 " + HPAtRPM * t);
+        //(a + (b - a)) * t;
+        //(0, HPAtRPM, Mathf.InverseLerp(0, ));
+        return ULerpedValue;
     }
 
     //Torque in N.m
     float CalculateTorque(float RPM)
     {
-        //return ((63.025f * horsepower) / RPM) * 1.3559179f;
+        //return ((63.025f * currentHP) / RPM) * 1.3559179f;
+
         //h = (t * r) / 5252
-        return horsepower * (5252 / RPM);
+        return currentHP * (5252 / RPM);
+    
     }
 
     void UpdateGear()
@@ -154,5 +178,5 @@ public class RPMCarController : MonoBehaviour
         }
 
     }
-    
+
 }
